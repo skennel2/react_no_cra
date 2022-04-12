724e9ac3f6196550577b69d268e0ee531127453b
skennel2 <skennel2@gmail.com>
2022-04-12 14:28:05 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function runApp() {
+    const container = document.getElementById('root');
+    if(container) {
+        const root = createRoot(container);
+        root.render(<ClickCounter name={'카운터'}/>);
+    
+        console.log("app started");
+    }
+}
+
+function ClickCounter(props: {name: string}) {
+    const [count, setCount] = useState(0);
+
+    const handleClick = useCallback((e: any) => {
+        console.log(e);
+        setCount(count + 1);
+    }, [count]);
+
+    return (
+        <span onClick={handleClick}>
+            {props.name} {count}
+        </span>
+    )
+}
+
+runApp();
